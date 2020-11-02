ac1fb28bae38bd5f54f9bc0b350b5917dcb07437
svetlana.kim <svetlana.kim.7008@gmail.com>
2020-11-02 19:52:17 +0700

    динамическое добавление компонентов в боди

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+  ViewChild,
+  Type,
+  AfterViewInit
+} from '@angular/core';
+
+import { DashboardComponent } from '../features/hello-world/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-body',
-  templateUrl: './body.component.html',
+  template: `
+    <div>
+      <ng-template #container></ng-template>
+    </div>
+  `,
   styleUrls: ['./body.component.less']
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent implements AfterViewInit {
+  childComponents: Type<any>[];
+  @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
 
-  constructor() { }
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.updateChildComponents();
+  }
+
+  updateChildComponents(): void {
+    this.childComponents = [DashboardComponent];
+    for (const componentType of this.childComponents){
+      this.addComponent(componentType);
+    }
+  }
+
+  addComponent(componentType: Type<any>): void {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(componentType);
+    const component = this.container.createComponent(componentFactory);
+    console.log(component);
   }
 
 }
