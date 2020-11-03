f3ab1fb2fb519e193eaab7d1d3a163be246f78cb
svetlana.kim <svetlana.kim.7008@gmail.com>
2020-11-03 20:00:55 +0700

    регистрация компонентов

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,16 +3,27 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ProgramComponent } from './program/program.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NotFoundComponent } from './not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, MainPageComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
+    ProgramComponent,
+    GalleryComponent,
+    NotFoundComponent
+  ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
-    MainPageComponent
+    MainPageComponent,
+    NotFoundComponent
   ]
 })
 export class CoreModule { }
