2905321866f8533ae166ed66bd9a650e7f3a9ae3
svetlana.kim <svetlana.kim.7008@gmail.com>
2020-11-11 13:44:30 +0700

    удалила неиспользуемый импорт

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ActiveModulesGuard } from './core/activeModules.guard';
-import { MainPageModule } from './features/main-page/main-page.module';
 
 const routes: Routes = [
   {
