f2cbb92043f9fb4eb65280003fac360a2c279945
svetlana.kim <svetlana.kim.7008@gmail.com>
2020-11-12 01:13:22 +0700

    добавила теги

--- src/app/features/gallery/gallery.component.ts
+++ src/app/features/gallery/gallery.component.ts
@@ -5,6 +5,16 @@ import { Gallery, GalleryItem } from 'ng-gallery';
 @Component({
   selector: 'app-gallery',
   template: `
+  <div class ='gallery_container'>
+    <div class = 'gallery_board'>
+    <nav class = 'gallery_nav'>
+      <ul class = 'nav'>
+        <li *ngFor="let item of menuItems" class='nav_item'>
+          <a routerLink={{item.path}} href="{{item.path}}" class='nav_link'>{{item.name}}</a>
+        </li>
+      </ul>
+    </nav>
+    </div>
     <ul class="mdc-image-list mdc-image-list--masonry my-masonry-image-list">
       <li *ngFor="let item of items,let i = index" class="mdc-image-list__item">
         <div class="grid-item"
@@ -13,14 +23,21 @@ import { Gallery, GalleryItem } from 'ng-gallery';
           <img [src]="item.data.thumb" class="mdc-image-list__image">
           </div>
       <li>
-    </ul> `,
+    </ul>
+</div>`,
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+
+  constructor(private galleryService: GalleryService, public gallery: Gallery) { }
   items: GalleryItem[] ;
   galleryId = 'my-gallery';
 
-  constructor(private galleryService: GalleryService, public gallery: Gallery) { }
+  menuItems = [
+    {name: '#teg1', path: '#teg1'},
+    {name: '#teg2', path: 'teg1'},
+    {name: '#teg3', path: 'teg1'}
+  ];
 
   ngOnInit(): void {
     this.items = this.galleryService.getGalleryItems();
@@ -28,4 +45,5 @@ export class GalleryComponent implements OnInit {
     const galleryRef = this.gallery.ref(this.galleryId);
     galleryRef.load(this.items);
   }
+
 }
