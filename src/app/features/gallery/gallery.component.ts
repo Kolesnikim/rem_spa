64b575a0ea8b635d0dd6cb0f0ef488565ebb7410
svetlana.kim <svetlana.kim.7008@gmail.com>
2020-11-12 00:31:18 +0700

    добавила стили для галлереи

--- src/app/features/gallery/gallery.component.ts
+++ src/app/features/gallery/gallery.component.ts
@@ -5,19 +5,20 @@ import { Gallery, GalleryItem } from 'ng-gallery';
 @Component({
   selector: 'app-gallery',
   template: `
-              <div class="grid-item"
-                *ngFor="let item of items; let i = index"
-                [lightbox]="i"
-                [gallery]="galleryId">
-                <img [src]="item.data.thumb">
-              </div>
-  `,
+    <ul class="mdc-image-list mdc-image-list--masonry my-masonry-image-list">
+      <li *ngFor="let item of items,let i = index" class="mdc-image-list__item">
+        <div class="grid-item"
+          [lightbox]="i"
+          [gallery]="galleryId">
+          <img [src]="item.data.thumb" class="mdc-image-list__image">
+          </div>
+      <li>
+    </ul> `,
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
   items: GalleryItem[] ;
   galleryId = 'my-gallery';
-
 
   constructor(private galleryService: GalleryService, public gallery: Gallery) { }
 
@@ -27,5 +28,4 @@ export class GalleryComponent implements OnInit {
     const galleryRef = this.gallery.ref(this.galleryId);
     galleryRef.load(this.items);
   }
-
 }
