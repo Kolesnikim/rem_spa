8152f556b8452e704a6ddb037bec9c7a75c4c37e
Kolesnikim <ilya.m.kolesnik@gmail.com>
2020-11-13 11:57:31 +0700

    implement table

--- src/app/features/timetable/timetable-routing.module.ts
+++ src/app/features/timetable/timetable-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TimetableComponent } from './timetable.component';
+import { TimetableComponent } from './components/timetable.component';
 
 const routes: Routes = [{ path: '', component: TimetableComponent }];
 
--- src/app/features/timetable/timetable.module.ts
+++ src/app/features/timetable/timetable.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+
 
 import { TimetableRoutingModule } from './timetable-routing.module';
-import { TimetableComponent } from './timetable.component';
+import { TimetableComponent } from './components/timetable.component';
 
 
 @NgModule({
   declarations: [TimetableComponent],
   imports: [
     CommonModule,
-    TimetableRoutingModule
+    TimetableRoutingModule,
+    MatTableModule,
+    MatButtonToggleModule,
+    MatButtonModule
   ]
 })
 export class TimetableModule { }
