0b6670f307a90f03ba905458ddca3dbd795f212a
svetlana.kim <svetlana.kim.7008@gmail.com>
2020-11-15 23:31:13 +0700

    добавила разделение по тегам

--- src/app/features/gallery/gallery.component.ts
+++ src/app/features/gallery/gallery.component.ts
@@ -10,23 +10,22 @@ import { Gallery, GalleryItem } from 'ng-gallery';
     <div class = 'gallery_board'>
     <nav class = 'gallery_nav'>
       <ul class = 'nav'>
-        <li *ngFor="let item of menuItems" class='nav_item'>
-          <a routerLink={{item.path}} href="{{item.path}}" class='nav_link'>{{item.name}}</a>
+        <li *ngFor="let tag of galleryTags" class='nav_item'>
+          <p class='nav_link' (click)='getItems(tag)'>{{tag}} </p>
         </li>
       </ul>
     </nav>
     </div>
     <div class='gallery_viewport'
     infiniteScroll
-  [infiniteScrollDistance]="1"
+  [infiniteScrollDistance]="5"
   [infiniteScrollThrottle]="300"
   (scrolled)="onScroll()"
   [scrollWindow]="false">
     <ul class="mdc-image-list my-image-list">
-      <li *ngFor="let item of items,let i = index" class="mdc-image-list__item">
-        <div class="mdc-image-list__image-aspect-container" [lightbox]="i" [gallery]="galleryId">
-          <img [src]="item.data.thumb" class="mdc-image-list__image">
-          <p>{{i}}</p>
+      <li *ngFor="let item of displayedItems,let i = index" class="mdc-image-list__item">
+        <div class="mdc-image-list__image-aspect-container" [id]=i [lightbox]="i - firstClickableIndex" [gallery]="galleryId">
+          <img (click)="onImageClick(i)" [src]="item.data.thumb" class="mdc-image-list__image">
         </div>
       <li>
     </ul>
@@ -35,27 +34,57 @@ import { Gallery, GalleryItem } from 'ng-gallery';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  galleryTags: string[];
+  displayedItems: GalleryItem[];
+  clickableItems: GalleryItem[];
+  allItems: GalleryItem[];
+  maxClickableItems = 100;
+  itemsPerScroll = 100;
+  galleryId = 'my-gallery';
+  firstClickableIndex = 0;
+  lastItemIndex: number = this.itemsPerScroll;
 
   constructor(private galleryService: GalleryService, public gallery: Gallery) { }
-  items: GalleryItem[];
-  galleryId = 'my-gallery';
-
-  menuItems = [
-    { name: '#teg1', path: '#teg1' },
-    { name: '#teg2', path: 'teg1' },
-    { name: '#teg3', path: 'teg1' }
-  ];
 
   ngOnInit(): void {
-    this.items = this.galleryService.getGalleryItems();
-
-    const galleryRef = this.gallery.ref(this.galleryId);
-    galleryRef.load(this.items);
+    this.galleryTags = this.galleryService.getGalleryTags();
+    if (this.galleryTags.length > 0){
+     this.getItems(this.galleryTags[0]);
+     console.log('all items size ' + this.allItems.length);
+    }
   }
 
   onScroll(): void {
-    console.log('Scrolled!!');
-    this.items.push(...this.items.slice(0, 100));
-    console.log('items size ' + this.items.length);
+    console.log('scroll');
+    this.lastItemIndex += this.itemsPerScroll;
+    if (this.lastItemIndex > this.allItems.length){
+      this.lastItemIndex = this.allItems.length;
+    }
+    this.displayedItems = this.allItems.slice(0, this.lastItemIndex);
+    console.log('displayed items ' + this.displayedItems.length);
+  }
+
+  onImageClick(id: number): void {
+    this.firstClickableIndex = id - this.maxClickableItems / 2;
+    if (this.firstClickableIndex < 0){
+      this.firstClickableIndex = 0;
+    }
+
+    console.log('id ' + id);
+    console.log('firstClickable ' + this.firstClickableIndex);
+
+    const galleryRef = this.gallery.ref(this.galleryId);
+    this.clickableItems = this.displayedItems.slice(this.firstClickableIndex, this.firstClickableIndex + this.maxClickableItems);
+    galleryRef.load(this.clickableItems);
+  }
+
+  getItems(tag: string): void {
+    this.firstClickableIndex = 0;
+    this.lastItemIndex = this.itemsPerScroll;
+    this.allItems = this.galleryService.getGalleryItems(tag);
+    this.displayedItems = this.allItems.slice(0, this.lastItemIndex);
+
+    const galleryRef = this.gallery.ref(this.galleryId);
+    galleryRef.load(this.displayedItems);
   }
 }
