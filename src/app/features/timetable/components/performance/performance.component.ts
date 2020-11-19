10cf90a670f6c3d342a377f85d27a624268f8111
Kolesnikim <ilya.m.kolesnik@gmail.com>
2020-11-19 10:28:50 +0700

    fix by style guide

--- src/app/features/timetable/components/performance/performance.component.ts
+++ src/app/features/timetable/components/performance/performance.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PerformanceService} from '../../services/performanceService/performance.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-performance',
