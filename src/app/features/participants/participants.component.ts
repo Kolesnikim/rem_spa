59f16bc5d1a8282c4db9e936c0f7d2ee7c3efb29
svetlana.kim <svetlana.kim.7008@gmail.com>
2020-11-20 19:45:24 +0700

    добавила теги в модуль

--- src/app/features/participants/participants.component.ts
+++ src/app/features/participants/participants.component.ts
@@ -2,10 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-participants',
-  templateUrl: './participants.component.html',
+  template:
+  `<div class='participans_container'>
+  <div class='participans_board'>
+    <nav class='participans_nav'>
+      <ul class='nav'>
+        <li class='nav_item'>
+          <p class ='nav_link'>#Участники </p>
+        </li>
+        <li class='nav_item'>
+          <p class ='nav_link'>#Организаторы </p>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <div class='participans_content'>
+
+  </div>
+</div>`,
   styleUrls: ['./participants.component.scss']
 })
 export class ParticipantsComponent implements OnInit {
+  tags: string[] = ['Участники', 'Организаторы'];
 
   constructor() { }
 
