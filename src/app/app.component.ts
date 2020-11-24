b43e2a8dcbf8a6c22a0840f363c79779eaa63c2f
Kolesnikim <ilya.m.kolesnik@gmail.com>
2020-11-24 10:57:54 +0700

    add app_initializer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import { HttpService } from './core/services/httpService/http.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   title = 'rem-spa';
-  constructor(private http: HttpService) {
-  }
 
-  ngOnInit(): any {
-    this.http.fetchAuthEnable().subscribe();
-  }
+  constructor() {}
+
 }
