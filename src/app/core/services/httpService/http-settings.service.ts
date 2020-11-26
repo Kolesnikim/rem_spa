5c2ad1c79dcf54a39b69da922a647d45236426f0
Kolesnikim <ilya.m.kolesnik@gmail.com>
2020-11-26 09:03:59 +0700

    fix bug with exception after empty response

--- src/app/core/services/httpService/http-settings.service.ts
+++ src/app/core/services/httpService/http-settings.service.ts
@@ -11,21 +11,21 @@ import { AuthService } from '../authService/auth.service';
  */
 @Injectable()
 export class HttpSettingsService {
-  private readonly authSettingsSubject: BehaviorSubject<IAuthEnable>;
+  private readonly authSettingsSubject: BehaviorSubject<IAuthEnable | null>;
   public url = environment.baseUrl;
 
   constructor(private http: HttpClient, private auth: AuthService) {
-    this.authSettingsSubject = new BehaviorSubject<IAuthEnable>(null);
+    this.authSettingsSubject = new BehaviorSubject<IAuthEnable | null>(null);
   }
 
-  public get getAuthEnable(): Subject<IAuthEnable> {
+  public get getAuthEnable(): Subject<IAuthEnable | null> {
     return this.authSettingsSubject;
   }
 
   public fetchAuthEnable(): Observable<void> {
-    return this.http.get<IAuthEnable>(`${environment.baseUrl}general-settings/get-auth-settings`)
+    return this.http.get<IAuthEnable | null>(`${environment.baseUrl}general-settings/get-auth-settings`)
       .pipe(map( res => {
-        this.authSettingsSubject.next(res);
+        this.authSettingsSubject.next(null);
       }));
   }
 }
