71477cb5ac3f75f8ab99030b7005067b40da6c5d
svetlana.kim <svetlana.kim@rubius.com>
2020-12-04 16:03:26 +0700

    табуляция в файле participants.module

--- src/app/features/participants/participants.module.ts
+++ src/app/features/participants/participants.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import { ParticipantsRoutingModule } from './participants-routing.module';
 import { ParticipantsComponent } from './participants.component';
 import { ParticipantCardComponent } from './components/participant-card/participant-card.component';
@@ -9,7 +9,11 @@ import { ParticipantDetailsComponent } from './components/participant-details/pa
 import { ParticipantSessionsComponent } from './components/participant-sessions/participant-sessions.component';
 
 @NgModule({
-  declarations: [ParticipantsComponent, ParticipantCardComponent, ParticipantDetailsComponent, ParticipantSessionsComponent],
+  declarations: [
+    ParticipantsComponent,
+    ParticipantCardComponent,
+    ParticipantDetailsComponent,
+    ParticipantSessionsComponent],
   imports: [
     CommonModule,
     ParticipantsRoutingModule,
