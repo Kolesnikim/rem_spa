1eefe02d4654f8a1c4e81c2641799d1f83e49e20
svetlana.kim <svetlana.kim@rubius.com>
2020-12-09 09:56:59 +0700

    стили и события модуля интересных мест

--- src/app/features/interesting-places/interesting-places.component.ts
+++ src/app/features/interesting-places/interesting-places.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YaReadyEvent } from 'angular8-yandex-maps';
 import { InterestingPlace } from './models/interesting-place.model';
 import { InterestingPlacesService } from './services/interesting-places.service';
 
@@ -6,21 +7,43 @@ import { InterestingPlacesService } from './services/interesting-places.service'
   selector: 'app-interesting-places',
   template: `
   <div class='content'>
-    <div class = 'interesting-cards_wrapper' *ngFor="let onePlece of interestingPlaces">
-      <app-interesting-place-card [place]="onePlece"></app-interesting-place-card>
-    </div>
+    <section class ='cards-wrapper'>
+      <header>
+        <img src='https://devpro.blob.core.windows.net/photos/places/томск.jpg'>
+        <h1>Чем заняться в Томске</h1>
+        <p>
+          Томск – город с богатой историей,
+          и в то же время очень молодёжный.
+          Томск - бывшая столица Сибири (основан в 1604 г.),
+           а его деревянное зодчество славится на весь мир.
+           А молодёжи на улицах города так много, потому что в Томске – 6 университетов.
+           Из 700 000 населения – около 100 000 студенты из 70 стран.
+           Также поговаривают, здесь самая высокая концентрация айтишников
+            на один квадратный метр. Что тоже не удивительно: в городе трудятся
+             больше 7 000 IT-специалистов в доброй сотне экспортирующих софтверных компаний.
+             В Томске есть куда сходить и что посмотреть.
+        </p>
+      </header>
+      <div class = 'interesting-card' *ngFor="let onePlace of interestingPlaces; let i = index" >
+        <app-interesting-place-card [place]="onePlace" id={{i}}></app-interesting-place-card>
+      </div>
+    </section>
     <div id = 'map' class='map'>
-      <ya-map [center]="[56.45, 84.96]" [zoom]="13">
-        <ya-clusterer [options]="clustererOptions" *ngFor="let onePlece of interestingPlaces; let i = index">
-          <ya-placemark [geometry]="coordinates[i]"
-          [properties]="{hintContent: onePlece.name,
-                        balloonContent: onePlece.name}">
+      <ya-map [center]="[56.48, 84.96]" [zoom]="13">
+        <ya-clusterer [options]="clustererOptions" *ngFor="let onePlace of interestingPlaces; let i = index">
+          <ya-placemark
+            [geometry]="coordinates[i]"
+            [properties]="{hintContent: onePlace.name, balloonContent: onePlace.name}"
+            (yamouseenter)="onMouseInter()"
+            (yamouseleave)="onMouseLeave()"
+            (yaclick)="onPlacemarkClick(i)"
+          >
           </ya-placemark>
         </ya-clusterer>
       </ya-map>
     </div>
   </div>
-  `,
+    `,
   styleUrls: ['./interesting-places.component.scss']
 })
 export class InterestingPlacesComponent implements OnInit {
@@ -32,11 +55,6 @@ export class InterestingPlacesComponent implements OnInit {
   };
   public coordinates = [];
 
-  // public placemarkProperties = {
-  //   hintContent: 'Hint content',
-  //   balloonContent: 'Baloon content',
-  // }
-
   constructor(private interestingOlacesService: InterestingPlacesService) { }
 
   ngOnInit(): void {
@@ -45,7 +63,6 @@ export class InterestingPlacesComponent implements OnInit {
     this.interestingOlacesService.getInterestingPlaces(this.idConference)
     .subscribe((interestingPlases: InterestingPlace[]) => {
       this.interestingPlaces = interestingPlases;
-      console.log(this.interestingPlaces);
 
       this.interestingPlaces.forEach(el => {
         onePlaceCoordinate.push(el.latitude),
@@ -57,4 +74,28 @@ export class InterestingPlacesComponent implements OnInit {
 
   }
 
+  /**
+   * обработать событие наведения мыши на объект
+   */
+  public onMouseInter(): void{
+    this.clustererOptions.preset = 'islands#greenIcon';
+  }
+
+  /**
+   * обработать событие отведения мыши с объекта
+   */
+  public onMouseLeave(): void{
+    this.clustererOptions.preset = 'islands#greenIcon';
+    // if (event.type === 'mouseleave') {
+    //   event.instance.options.unset('preset');
+  }
+
+  /**
+   * обработать событие клика по объекту
+   */
+  public onPlacemarkClick(e: any): void {
+    e.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+    console.log(e);
+
+  }
 }
